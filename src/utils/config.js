8ff285ad71909ae018ba1287dba3b4f968508e78
Ian Chen <r05921035@ntu.edu.tw>
2018-08-09 01:38:16 +0800

    Update config.js interface

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,24 +1,30 @@
 import YAML from 'js-yaml'
 
-// (JSON/TOML)-formatted string -> Object
-export function parse(s, format = 'json') {
-  if (format === 'yaml') {
-    // TODO: post-parsing procedures
-    return YAML.safeLoad(s)
-  }
-  return JSON.parse(s)
+// YAML-formatted string -> Object
+export function parseChart(s) {
+  const obj = YAML.safeLoad(s)
+
+  if (obj.end === 'now') delete obj.end
+
+  return obj
 }
 
-// Object -> (JSON/TOML)-formatted string
-export function stringify(obj, format = 'json') {
-  if (format === 'yaml') {
-    // TODO: post-stringify procedures
-    return YAML.safeDump(obj)
-  }
+// Object -> YAML-formatted string
+export function stringifyChart(obj) {
+  return YAML.safeDump(obj)
+}
+
+export function stringifyEnvironment(obj) {
   return JSON.stringify(obj)
 }
 
+export function parseEnvironment(s) {
+  return JSON.parse(s)
+}
+
 export default {
-  parse,
-  stringify,
+  parseChart,
+  stringifyChart,
+  parseEnvironment,
+  stringifyEnvironment,
 }
